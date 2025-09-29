3db9118de31f8274945315fef202f109a74b8faa
MonsoudZ <monsoudz@users.noreply.github.com>
2025-09-29 16:54:36 -0400

    fix testing issues

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ApplicationMailer
-  default to: ENV.fetch("CONTACT_TO_EMAIL")
+  default to: ENV.fetch("CONTACT_TO_EMAIL", "test@example.com")
 
   def contact_email(name:, email:, message:)
     @name, @email, @message = name, email, message
     mail(
-      from: ENV.fetch("CONTACT_FROM_EMAIL"),
+      from: ENV.fetch("CONTACT_FROM_EMAIL", "noreply@example.com"),
       reply_to: email,
       subject: "New website inquiry from #{name}"
     )
